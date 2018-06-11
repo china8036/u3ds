7c4252fea3e5807747f3f6b87561ffc6b0efb7a5
wangzhenhong <wangzhenhong@pipaw.net>
2018-06-11 20:34:23 +0800

    positon step 2

--- src/main/java/com/witgame/u3d/controller/Position.java
+++ src/main/java/com/witgame/u3d/controller/Position.java
@@ -1,7 +1,16 @@
 package com.witgame.u3d.controller;
 
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.json.JSONObject;
+
 import com.witgame.u3d.Controller;
+import com.witgame.u3d.Redis;
 import com.witgame.u3d.Request;
 import com.witgame.u3d.Response;
 import com.witgame.u3d.Session;
@@ -9,14 +18,33 @@ import com.witgame.u3d.Session;
 public class Position extends Controller {
 
 
-	
-	
 	public void run(Request request, Response response, Session session) {
 		
 		try {
-			System.out.println(request.get("x"));
-			System.out.println(request.get("y"));
-			System.out.println(request.get("z"));
+			String sid = request.getSid();
+			Redis.getInstance().sadd("clients", sid);
+			JSONObject position = new JSONObject();
+			position.put("x", request.get("x"));
+			position.put("y", request.get("y"));
+			position.put("z", request.get("z"));
+			Map<String, String> postionMap = new HashMap<String, String>();
+			postionMap.put("position", position.toString());
+			Redis.getInstance().hmset(sid, postionMap);
+			Set<String> clients = Redis.getInstance().smembers("clients");
+			 for( Iterator<String>   it = clients.iterator();  it.hasNext(); )
+		        {           
+				   String tsid = it.next();
+		            if(sid.equals(tsid)) {
+		            	continue;
+		            }    
+		            if(Redis.getInstance().hexists(tsid, "position")){
+		            	Redis.getInstance().srem("clients", tsid);
+		            	continue;
+		            }
+		           String tposition = Redis.getInstance().hmget(tsid, "position").get(0);
+		           response.responseOk(new JSONObject(tposition));
+		    } 
+			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
